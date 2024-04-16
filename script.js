12ce5df843ff4405df79dae4bc8db4912e4cae91
venkatjamjhyam <167172235+venkatjamjhyam@users.noreply.github.com>
2024-04-16 12:45:31 +0530

    v1

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Code to handle form submission (e.g., send data to backend or display a message)
+    alert('Form submitted!');
+  });
+  
